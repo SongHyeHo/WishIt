19ed46cf9467875c26e5dd3f44a2371716e5c723
SongHyeHo <hyeho331@gmail.com>
2019-05-24 10:18:11 +0900

    Wish It: Add DataSet1 Page
    Signed-off-by: SongHyeHo <hyeho331@gmail.com>

--- wishit/app/src/main/java/com/portfolio/wish_it/DataSet1.java
+++ wishit/app/src/main/java/com/portfolio/wish_it/DataSet1.java
@@ -1,10 +1,26 @@
 package com.portfolio.wish_it;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.text.DecimalFormat;
 
 public class DataSet1 extends Activity {
+    View ln1, ln2;
+    EditText pday, salary;
+    String result = "";
+    //화폐단위 출력용 변수
+    DecimalFormat df = new DecimalFormat("#,###");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,5 +29,63 @@ public class DataSet1 extends Activity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_dataset_1);
+
+        //PayDay 입력 뷰 가져오기
+        ln1 = (View) findViewById(R.id.uline_payday);
+        ln2 = (View) findViewById(R.id.uline_salary);
+        pday = (EditText) findViewById(R.id.edt_payday);
+        salary = (EditText) findViewById(R.id.edt_salary);
+
+        //입력 값변경 이벤트 추가
+        pday.addTextChangedListener(
+                new TextWatcher() {
+                    @Override
+                    public void afterTextChanged(Editable edit) {
+                        //밑줄 색 변경
+                        String s = edit.toString();
+                        if (s.length() > 0)
+                            ln1.setBackgroundColor(Color.rgb(255, 184, 51));
+                        else
+                            ln1.setBackgroundColor(Color.rgb(217, 217, 217));
+                    }
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                    }
+                    @Override
+                    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                    }
+                });
+
+        salary.addTextChangedListener(
+                new TextWatcher() {
+                    @Override
+                    public void afterTextChanged(Editable edit) {
+                        //밑줄 색 변경
+                        String s = edit.toString();
+                        if (s.length() > 0)
+                            ln2.setBackgroundColor(Color.rgb(255, 184, 51));
+                        else
+                            ln2.setBackgroundColor(Color.rgb(217, 217, 217));
+                    }
+
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence s, int start, int before, int count) {
+                        //화폐단위로 출력
+                        if(!TextUtils.isEmpty(s.toString()) && !s.toString().equals(result)) {
+                            result = df.format(Double.parseDouble(s.toString().replaceAll(",", "")));
+                            salary.setText(result);
+                            salary.setSelection(result.length());
+                        }
+                    }
+                });
     }
+
+
 }
--- wishit/app/src/main/java/com/portfolio/wish_it/InfoPage.java
+++ wishit/app/src/main/java/com/portfolio/wish_it/InfoPage.java
@@ -1,8 +1,11 @@
 package com.portfolio.wish_it;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class InfoPage extends Activity {
     @Override
@@ -13,5 +16,11 @@ public class InfoPage extends Activity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_info);
+    }
+
+    //"알겠습니다" 버튼 클릭 이벤트
+    public void onClick(View v) {
+        //데이터입력1 페이지로 이동
+        startActivity(new Intent(this, DataSet1.class));
     }
 }
